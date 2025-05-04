58d2d91da7bd6cdf3f2a9a919d1b3c0b7b0c9513
nitin yadav <nitinyadav484220@gmail.com>
2025-05-04 21:46:11 +0530

    changes

---
+++ turbo/chat-app/apps/web/app/page.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import {TextInput} from "@repo/ui/text-input"
+import styles from "./page.module.css";
+import { useRouter } from "next/navigation";
+
+export default function Home(){
+  
+  const router = useRouter();
+
+  return (
+    <div style={{height:"100vh" , width:"100vw" , background:"black" , display:"flex"  , justifyContent:"center",justifyItems:"center"}}>
+         <div style={{display:"flex" , justifyContent:"center", flexDirection:"column"}}>
+       
+        <TextInput onChange={()=>{
+
+        }}  placeholder="end the room" ></TextInput>
+        <button style={{padding:"10px" , fontSize:"10px" ,border:"2px . solid , black" , borderRadius:"24px" }}  onClick={()=>{
+          router.push("/chat/123")
+        }}>Join us</button>
+         </div>
+  </div>)
+}
---
+++ turbo/chat-app/packages/ui/src/text-input.tsx
@@ -0,0 +1,21 @@
+import { prototype } from "events"
+
+interface PropType{
+   placeholder: string,
+   size: "big" | "small"
+}
+
+
+export function TextInput({
+    placeholder,
+    size
+}:PropType){
+    return <input  placeholder={placeholder }  style={{
+        padding: size === "big" ? 20:10,
+        margin:size === "big" ? 20:10,
+        borderColor:"black",
+        borderWidth: 1,
+       
+    }}>
+    </input>
+}
