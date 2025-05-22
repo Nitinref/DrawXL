879aafcbea37ccaa710a918156d502347509511c
nitin yadav <nitinyadav484220@gmail.com>
2025-05-22 23:24:32 +0530

    added icons

--- drawxl/apps/excelidraw-frontend/components/Canvas.tsx
+++ drawxl/apps/excelidraw-frontend/components/Canvas.tsx
@@ -2,6 +2,8 @@ import { initDraw } from "@/draw"
 import { Socket } from "dgram";
 import { useEffect } from "react"
 import { useRef } from "react";
+import { IconButton } from "./IconButton";
+import { Circle, Pencil, PencilIcon, RectangleEllipsis, RectangleHorizontal } from "lucide-react";
 export function Canvas({
     roomId,
     socket
@@ -28,6 +30,22 @@ export function Canvas({
             overflow:"hidden"
         }}>
             <canvas ref={canvasRef} width={window.innerWidth} height={window.innerHeight} ></canvas>
+            <Topbar />
         </div>
     )
+}
+
+
+function Topbar() {
+    return  <div style={{
+        position:"fixed",
+        top:10,
+        left:10
+    }}>
+        <IconButton icon={<PencilIcon />}  onClick={()=>{}}/>
+        <IconButton icon={<RectangleHorizontal className=""/>}  onClick={()=>{}}/> 
+        <IconButton icon={<Circle />}  onClick={()=>{}}/>
+
+
+    </div>
 }
---
+++ drawxl/apps/excelidraw-frontend/components/IconButton.tsx
@@ -0,0 +1,15 @@
+import { LineChart } from "lucide-react";
+import { ReactNode } from "react";
+
+export function IconButton({
+    icon, onClick
+}: {
+    icon: ReactNode,
+    onClick: () => void
+}) {
+
+    return <div className="pointer rounded-full border p-2 bg-black hover:bg-blue-300 text-white">
+
+        {icon}
+    </div>
+}
